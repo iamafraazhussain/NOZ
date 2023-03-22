d44227b43fbb663e01fb1d2c63596189129f87fb
Afraaz Hussain <46090374+iamafraazhussain@users.noreply.github.com>
2023-03-22 20:00:14 +0530

    Update systemVariables.py

--- systemVariables.py
+++ systemVariables.py
@@ -1,138 +1,138 @@
-class systemVariables:
-    
-    appDimension = [900, 500]
-    
-    stylesheet = '''
-    
-    QMainWindow
-    {
-        background-color: #f5f3e9;
-    }
-    
-    
-    
-    #backgroundImage
-    {
-        ;
-    }
-    
-    
-    
-    #mainContainerWidget
-    {
-        background-color: #f5f3e9;        
-        border-radius: 10px;
-        font-family: 'Nirmala UI';
-        font-size: 14px;
-        padding: 5px;
-        color: #333333;
-    }
-    
-    #mainContainerWidget:placeholder
-    {
-        color: #777777;
-    }
-    
-    
+    class systemVariables:
+        
+        appDimension = [900, 500]
+        
+        stylesheet = '''
+        
+        QMainWindow
+        {
+            background-color: #f5f3e9;
+        }
+        
+        
+        
+        #backgroundImage
+        {
+            ;
+        }
+        
+        
+        
+        #mainContainerWidget
+        {
+            background-color: #f5f3e9;        
+            border-radius: 10px;
+            font-family: 'Nirmala UI';
+            font-size: 14px;
+            padding: 5px;
+            color: #333333;
+        }
+        
+        #mainContainerWidget:placeholder
+        {
+            color: #777777;
+        }
+        
+        
 
-    #subContainerWidget
-    {
-        border-radius: 10px;
-    }
-    
-    
-    
-    #scrollableWidget
-    {
-        background-color: rgba(0, 0, 0, 0);
-        border-color: rgba(0, 0, 0, 0);
-        border-width: 0px;
-        border-radius: 10px;
-    }
-    
-    
-    
-    #subContainerLabel
-    {
-        color: #050505;
-        font-family: 'Nirmala UI';
-        font-size: 16px;
-        font-weight: 550;
-    }
-    
-    
-    
-    #defaultAlterButton
-    { 
-        background-color: #333333;
-        border-radius: 10px;
-        font-family: 'Nirmala UI';
-        font-size: 9px;
-        padding: 5px;
-        color: #f5f3e9;
-    }
-    
-    #defaultAlterButton:hover
-    {
-        background-color: #f5f3e9;
-        color: #333333
-    }
-    
-    #defaultAlterButton:checked
-    {
-        background-color: #f5f3e9;
-        color: #333333;
-    }
-    
-    
-    
-    #defaultButton
-    { 
-        background-color: #e9e2d7;
-        border-radius: 10px;
-        font-family: 'Nirmala UI';
-        font-size: 10px;
-        padding: 5px;
-        padding-left: 10px;
-        padding-right: 10px;
-        color: #333333;
-    }
-    
-    #defaultButton:hover
-    {
-        background-color: #333333;
-        color: #f5f3e9;
-    }
-    
-    #defaultButton:checked
-    {
-        background-color: #333333;
-        color: #f5f3e9;
-    }
-    
-    
-    
-    #queryContainer
-    {
-        background-color: #333333;
-        border-radius: 10px;
-        font-family: 'Nirmala UI';
-        font-size: 9px;
-        padding: 5px;
-        padding-left: 25px;
-        padding-right: 10px;
-        color: #e9e2d7;
-    }
-    
-    
-    
-    #errorMessage
-    {
-        background-color: #FF4010;
-        border-radius: 10px;
-        font-family: 'Nirmala UI';
-        font-size: 10px;
-        padding: 5px;
-        color: #f5f3e9;
-    }
-    '''
+        #subContainerWidget
+        {
+            border-radius: 10px;
+        }
+        
+        
+        
+        #scrollableWidget
+        {
+            background-color: rgba(0, 0, 0, 0);
+            border-color: rgba(0, 0, 0, 0);
+            border-width: 0px;
+            border-radius: 10px;
+        }
+        
+        
+        
+        #subContainerLabel
+        {
+            color: #050505;
+            font-family: 'Nirmala UI';
+            font-size: 16px;
+            font-weight: 550;
+        }
+        
+        
+        
+        #defaultAlterButton
+        { 
+            background-color: #333333;
+            border-radius: 10px;
+            font-family: 'Nirmala UI';
+            font-size: 9px;
+            padding: 5px;
+            color: #f5f3e9;
+        }
+        
+        #defaultAlterButton:hover
+        {
+            background-color: #f5f3e9;
+            color: #333333
+        }
+        
+        #defaultAlterButton:checked
+        {
+            background-color: #f5f3e9;
+            color: #333333;
+        }
+        
+        
+        
+        #defaultButton
+        { 
+            background-color: #e9e2d7;
+            border-radius: 10px;
+            font-family: 'Nirmala UI';
+            font-size: 10px;
+            padding: 5px;
+            padding-left: 10px;
+            padding-right: 10px;
+            color: #333333;
+        }
+        
+        #defaultButton:hover
+        {
+            background-color: #333333;
+            color: #f5f3e9;
+        }
+        
+        #defaultButton:checked
+        {
+            background-color: #333333;
+            color: #f5f3e9;
+        }
+        
+        
+        
+        #queryContainer
+        {
+            background-color: #333333;
+            border-radius: 10px;
+            font-family: 'Nirmala UI';
+            font-size: 9px;
+            padding: 5px;
+            padding-left: 25px;
+            padding-right: 10px;
+            color: #e9e2d7;
+        }
+        
+        
+        
+        #errorMessage
+        {
+            background-color: #FF4010;
+            border-radius: 10px;
+            font-family: 'Nirmala UI';
+            font-size: 10px;
+            padding: 5px;
+            color: #f5f3e9;
+        }
+        '''
