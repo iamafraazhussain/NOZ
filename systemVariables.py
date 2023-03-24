00112f395613be1e48de7f3dd5c10073d7542963
Afraaz Hussain <46090374+iamafraazhussain@users.noreply.github.com>
2023-03-24 22:17:31 +0530

    Update systemVariables.py

--- systemVariables.py
+++ systemVariables.py
@@ -4,20 +4,6 @@ class systemVariables:
     
     stylesheet = '''
     
-    QMainWindow
-    {
-        background-color: #f5f3e9;
-    }
-    
-    
-    
-    #backgroundImage
-    {
-        ;
-    }
-    
-    
-    
     #mainContainerWidget
     {
         background-color: #f5f3e9;        
@@ -31,6 +17,24 @@ class systemVariables:
     #mainContainerWidget:placeholder
     {
         color: #777777;
+    }
+    
+    
+    
+    #alternateContainerWidget
+    {
+        background-color: #f5f3e9;        
+        border-radius: 10px;
+        font-family: 'Nirmala UI';
+        font-size: 10px;
+        padding-left: 5px;
+        padding-right: 5px;
+        color: #333333;
+    }
+    
+    #alternateContainerWidget:disabled
+    {
+        background-color: rgba(255, 255, 255, 0.5);
     }
     
     
@@ -54,6 +58,7 @@ class systemVariables:
     
     #subContainerLabel
     {
+        background-color: rgba(0, 0, 0, 0);
         color: #050505;
         font-family: 'Nirmala UI';
         font-size: 16px;
@@ -110,6 +115,11 @@ class systemVariables:
         color: #f5f3e9;
     }
     
+    #defaultButton:disabled
+    {
+        background-color: rgba(255, 255, 255, 0.5);
+    }
+    
     
     
     #queryContainer
@@ -134,5 +144,17 @@ class systemVariables:
         font-size: 10px;
         padding: 5px;
         color: #f5f3e9;
+    }
+    
+    
+    
+    #relevanceScore
+    {
+        background-color: #333333;
+        border-radius: 10px;
+        font-family: 'Nirmala UI';
+        font-size: 9px;
+        padding: 5px;
+        color: #e9e2d7;
     }
     '''
